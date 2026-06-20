284686692100b93a48b3d9612f78c4c4fce649f7
justciphertv <chrsong095@gmail.com>
2026-06-20 11:19:44 -0500

    Update headers.ts

--- src/headers.ts
+++ src/headers.ts
@@ -91,6 +91,11 @@ export const DOMAIN_GROUPS: DomainGroup[] = [
         referer: "https://vidwish.live/",
     },
     {
+        patterns: [/(?:^|\.)as-cdn21\./i],
+        origin: "https://as-cdn21.top",
+        referer: "https://as-cdn21.top/",
+    },
+    {
         patterns: [/(?:^|\.)dotstream\./i, /(?:^|\.)playcloud1\./i, /(?:^|\.)mewstream\./i, /(?:^|\.)sparqle\./i, /(?:^|\.)zapora\./i, /(?:^|\.)cinewave2\./i, /(?:^|\.)cinewave2\./i, /(?:^|\.)lumiflow\./i, /(?:^|\.)flarestorm\./i, /(?:^|\.)ibyteimg\./i, /(?:^|\.)lostproject\./i],
         origin: "https://megaplay.buzz/",
         referer: "https://megaplay.buzz/",
