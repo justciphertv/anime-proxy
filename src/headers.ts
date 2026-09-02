edc4aff2072d40db062b33ab86a2340df650696a
justciphertv <chrsong095@gmail.com>
2026-09-01 22:56:44 -0500

    Update headers.ts

--- src/headers.ts
+++ src/headers.ts
@@ -156,6 +156,11 @@ export const DOMAIN_GROUPS: DomainGroup[] = [
         referer: "https://player.videasy.to/",
     },
     {
+        patterns: [/\.crispglobe\.top/i],
+        origin: "https://player.videasy.to",
+        referer: "https://player.videasy.to/",
+    },
+    {
         patterns: [/\.streamcdn\.com$/i],
         origin: "https://anime.uniquestream.net",
         referer: "https://anime.uniquestream.net/",
